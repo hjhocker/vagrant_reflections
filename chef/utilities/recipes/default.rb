9460a9f5d57a324984e07a72efb48aa8dcf3d4e1
Harrison Hocker <hjhocker@gmail.com>
2018-09-29 17:45:45 -0500

    postgres

--- chef/utilities/recipes/default.rb
+++ chef/utilities/recipes/default.rb
@@ -27,3 +27,38 @@ postgresql_server_install 'Setup my postgresql 9.6 server' do
   port 5432
   action :create
 end
+
+# https://bosnadev.com/2015/12/15/allow-remote-connections-postgresql-database-server/
+postgresql_access 'local_postgres_superuser' do
+  comment 'Local postgres superuser access'
+  access_type 'local'
+  access_db 'all'
+  access_user 'postgres'
+  access_addr nil
+  access_method 'ident'
+end
+
+postgresql_user 'reflections_user' do
+  password 'harrison'
+  createdb true
+end
+
+postgresql_access 'reflections_user' do
+  comment 'Foo user access'
+  access_type 'host'
+  access_db 'all'
+  access_user 'reflections_user'
+  access_addr '0.0.0.0/0'
+  access_method 'md5'
+end
+
+template '/etc/postgresql/9.6/main/postgresql.conf' do
+  source 'postgresql.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+service "postgresql" do
+  action :restart
+end
